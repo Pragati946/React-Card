f840f84daedf4ffaecb0d7f69ce9d236b476df35
RupeshSahu <75201337+RupeshSahu969@users.noreply.github.com>
2022-05-18 18:29:12 +0530

    add pages

---
+++ src/components/card1.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+
+export const Card1 = () => {
+  
+    return (
+      <div>
+          
+          <div className='card'>
+          <div>
+            <div className='card1'>28/10/2020</div>
+            <div className='card2'>Case Study</div>
+            <h1 className='card1'>Amazon Gift</h1>
+            <h1 className='card1'>pay</h1>
+            <div className='card1'>Desktop - Mobile</div>
+            </div>
+            <div><img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6Hmfk0FT_SuS3KBAwxdTOgx01aLJih-h7qg&usqp=CAU'></img>
+            <img className='image' src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAP8AAADFCAMAAACsN9QzAAAAb1BMVEX///8GAAAAAACKiopxcXH09fUnJSbq6ur4+PjU1NR6eHmYl5d1dXXt7e38/Pw9PT2SkpJnZma9vLxPT08vLCzl5ORaV1izsrIXFhbJycnc3NxJRkarq6sZExClpaXAv7+DgoI1NDVgYGATEhNUVFTF7e78AAAB70lEQVR4nO3ai3ISMRQGYDZcrG21C62VVuxN3/8Z3YvoVqGdYXcI2XzfC5D/DOEkJ0wmAAAAAAAAAAAAAAAAAAAAAAAAwB7lxYfYS4hqFsJyNY+9inhmoQhhcf4x9jpiqfIXVQXC1cWX2EuJosnfluB6FXsxEWzzNxUI+e2Dv/nbEtxktg9e5W9LcPv1U+xVHc+/+ZsSrC/vzmIv7Eh25G/3QXkfe2lHsTN/W4JvmwzORfvyt/1geTfsp83DydkX/3cNvk+H3Afzxdufd3qqCl0/DNYP0stfNCVYPg7zU5Bk/qYCi6chjoaJ5i+aEjz33wfp5m/7we3qc7b5a1UJnu57lCD1/O2o4OHgK1L6+YumBC+rw6aGo8jfdsSDGuIY8tfN8NChcfL56ybQ4ySQdv7Q7PyDwyeev74JbHo+liSbvz7+DXATTDR/dUs+H2QilmL+ZtMPNAmar4893njXu+Fvhrv+T87K6Wkpr94oQBV+Pej45wRd7stfhf+xfOx33UvAnvlnlf7nJodnkF35q/DPZSbPYP/nD/Uz6Oi/91uv89cNoe+MJy3d/KGebeSw6Tv+5K87/TS75/9t/qrZzbJ58+2q84fM3vy7ZiG//3x0LfuMc0Zg5Md7AAAAAAAAAAAAAAAAAAAAAAAAAMbqF2TKHDeBXUl7AAAAAElFTkSuQmCC'></img></div>
+            
+          </div>
+      </div>
+    )
+  
+}
